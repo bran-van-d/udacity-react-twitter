2915fd4f2ef931ff59a61d26a6c98cac88abd857
Brandon Van Dyck <bvandyck@shyftanalytics.com>
2019-01-06 12:23:41 -0500

    feat: display proper data in the tweet component and incorporate a loading bar

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { formatTweet } from '../utils/helpers';
+import { formatTweet, formatDate } from '../utils/helpers';
+import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from "react-icons/ti";
 
 class Tweet extends Component {
+  toParent = (e, id) => {
+    e.preventDefault
+    // todo: redirect to parent Tweet.
+
+  }
+
+  handleLike = (e) => {
+    e.preventDefault
+    // todo: handle liking tweet
+  }
+
 
   render() {
-  console.log(this.props);
+    const { tweet } = this.props;
+
+    if (tweet === null) {
+      return <p> This tweet doesn't exist. </p>
+    }
+
+    const {
+      name, avatar, timestamp, hasLiked, likes, replies, parent, text
+    } = tweet;
+
+
     return (
       <div className="tweet"> 
+        <img
+          src={avatar}
+          alt={`Avatar of ${name}`}
+          className='avatar' />
+
+          <div className="tweet-info">
+            <div>
+              <span> {name} </span>
+              <div> {formatDate(timestamp)} </div>
+
+              {parent && (
+                <button className="replying-to" onClick={(e) => this.toParent(e, parent.id)}>
+                  Replaying to @{parent.author}
+                </button>
+              )}
+              <p> {text} </p>
+            </div>
+            <div className="tweet-icons">
+              <TiArrowBackOutline className="tweet-icon" />
+              <span> {replies !== 0 && replies} </span> 
+              <button className='heart-button' onClick={this.handleLike}>
+                {hasLiked === true
+                  ? <TiHeartFullOutline color="#e0245e" className="tweet-icon" />
+                  : <TiHeartOutline className="tweet-icon" />
+                }
+              </button>
+              <span> {likes !== 0 && likes} </span>
+            </div>
+          </div>
       </div>
     )
   }
@@ -15,10 +66,13 @@ class Tweet extends Component {
 
 function mapStateToProps({ authedUser, users, tweets }, { id }) {
   const tweet = tweets[id];
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
 
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser)
+    tweet: tweet 
+    ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+    : null
   }
 }
 
